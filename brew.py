da8c13fffe7efffa2ff1aef3b57d5d56bea4a123
d12frosted <d12frosted@gmail.com>
2016-06-28 12:12:05 +0300

    use one class for handing brew and cask

--- brew.py
+++ brew.py
@@ -1,43 +1,57 @@
 import os, subprocess, dotbot
 
 class Brew(dotbot.Plugin):
-    _directive = "brew"
+    _brewDirective = "brew"
+    _caskDirective = "cask"
 
     def can_handle(self, directive):
-        return directive == self._directive
+        return directive == self._brewDirective or directive == self._caskDirective
 
     def handle(self, directive, data):
-        if directive != self._directive:
-            raise ValueError('Brew cannot handle directive %s' %
-                directive)
-        return self._process_commands(data)
+        if directive == self._brewDirective:
+            self._bootstrap_brew()
+            return self._process_data("brew install", data)
+        if directive == self._caskDirective:
+            self._bootstrap_cask()
+            return self._process_data("brew cask install", data)
+        raise ValueError('Brew cannot handle directive %s' % directive)
 
-    def _process_commands(self, data):
-        success = True
-        defaults = self._context.defaults().get('shell', {})
-        self.bootstrap(self)
-        with open(os.devnull, 'w') as devnull:
-            for item in data:
-                stdin = stdout = stderr = devnull
-                cmd = "brew install %s" % item
-                self._log.info("Installing %s" % item)
-                ret = subprocess.call(cmd, shell=True, stdin=stdin, stdout=stdout,
-                    stderr=stderr, cwd=self._context.base_directory())
-                if ret != 0:
-                    success = False
-                    self._log.warning('Failed to install [%s]' % item)
+    def _process_data(self, install_cmd, data):
+        success = self._install(install_cmd, data)
         if success:
             self._log.info('All packages have been installed')
         else:
             self._log.error('Some packages were not installed')
         return success
 
-    @staticmethod
-    def bootstrap(self):
+    def _install(self, install_cmd, packages_list):
+        cwd = self._context.base_directory()
+        log = self._log
+        with open(os.devnull, 'w') as devnull:
+            stdin = stdout = stderr = devnull
+            for package in packages_list:
+                log.info("Installing %s" % package)
+                cmd = "%s %s" % (install_cmd, package)
+                result = subprocess.call(cmd, shell=True, stdin=stdin, stdout=stdout, stderr=stderr, cwd=cwd)
+                if result != 0:
+                    log.warning('Failed to install [%s]' % package)
+                    return False
+            return True
+
+    def _bootstrap(self, cmd):
+        with open(os.devnull, 'w') as devnull:
+            stdin = stdout = stderr = devnull
+            subprocess.call(cmd, shell=True, stdin=stdin, stdout=stdout, stderr=stderr,
+                            cwd=self._context.base_directory())
+
+    def _bootstrap_brew(self):
         cmd = """hash brew || {
           ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
           brew update
         }"""
-        with open(os.devnull, 'w') as devnull:
-            stdin = stdout = stderr = devnull
-            subprocess.call(cmd, shell=True, stdin=stdin, stdout=stdout, stderr=stderr)
+        self._bootstrap(cmd)
+
+    def _bootstrap_cask(self):
+        self._bootstrap_brew()
+        cmd = "brew tap caskroom/cask"
+        self._bootstrap(cmd)
