09e2bcd0bb5b4c6be57784f6719ee2f405c4740d
Björn Larsson <b.e.larsson@gmail.com>
2016-10-30 21:27:24 +0100

    Added support for tap key word

--- brew.py
+++ brew.py
@@ -3,11 +3,15 @@ import os, subprocess, dotbot
 class Brew(dotbot.Plugin):
     _brewDirective = "brew"
     _caskDirective = "cask"
+    _tapDirective = "tap"
 
     def can_handle(self, directive):
-        return directive == self._brewDirective or directive == self._caskDirective
+        return directive in (self._tapDirective, self._brewDirective, self._caskDirective)
 
     def handle(self, directive, data):
+        if directive == self._tapDirective:
+            self._bootstrap_brew()
+            return self._tap(data)
         if directive == self._brewDirective:
             self._bootstrap_brew()
             return self._process_data("brew install", data)
@@ -15,6 +19,22 @@ class Brew(dotbot.Plugin):
             self._bootstrap_cask()
             return self._process_data("brew cask install", data)
         raise ValueError('Brew cannot handle directive %s' % directive)
+
+    def _tap(self, tap_list):
+        cwd = self._context.base_directory()
+        log = self._log
+
+        with open(os.devnull, 'w') as devnull:
+            stdin = stdout = stderr = devnull
+            for tap in tap_list:
+                log.info("Tapping %s" % tap)
+                cmd = "brew tap %s" % (tap)
+                result = subprocess.call(cmd, shell=True, stdin=stdin, stdout=stdout, stderr=stderr, cwd=cwd)
+
+                if result != 0:
+                    log.warning('Failed to tap [%s]' % tap)
+                    return False
+            return True
 
     def _process_data(self, install_cmd, data):
         success = self._install(install_cmd, data)
@@ -30,10 +50,10 @@ class Brew(dotbot.Plugin):
         with open(os.devnull, 'w') as devnull:
             stdin = stdout = stderr = devnull
             for package in packages_list:
-                log.info("Installing %s" % package)
                 cmd = "brew --cellar %s" % package
                 isInstalled = subprocess.call(cmd, shell=True, stdin=stdin, stdout=stdout, stderr=stderr, cwd=cwd)
                 if isInstalled != 0:
+                    log.info("Installing %s" % package)
                     cmd = "%s %s" % (install_cmd, package)
                     result = subprocess.call(cmd, shell=True, stdin=stdin, stdout=stdout, stderr=stderr, cwd=cwd)
                     if result != 0:
