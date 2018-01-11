bcafbda01bab923befae6bcd54a2d5d281727ca4
Angelo Rüggeberg <s3xy4ngyc@googlemail.com>
2018-01-11 16:05:42 +0100

    Added Brewfile capabilities

--- brew.py
+++ brew.py
@@ -4,9 +4,10 @@ class Brew(dotbot.Plugin):
     _brewDirective = "brew"
     _caskDirective = "cask"
     _tapDirective = "tap"
+    _brewFileDirective = "brewfile"
 
     def can_handle(self, directive):
-        return directive in (self._tapDirective, self._brewDirective, self._caskDirective)
+        return directive in (self._tapDirective, self._brewDirective, self._caskDirective, self._brewFileDirective)
 
     def handle(self, directive, data):
         if directive == self._tapDirective:
@@ -18,12 +19,14 @@ class Brew(dotbot.Plugin):
         if directive == self._caskDirective:
             self._bootstrap_cask()
             return self._process_data("brew cask install", data)
+        if directive == self._brewFileDirective:
+            self._bootstrap_brew()
+            return self._install_bundle(data)
         raise ValueError('Brew cannot handle directive %s' % directive)
 
     def _tap(self, tap_list):
         cwd = self._context.base_directory()
         log = self._log
-
         with open(os.devnull, 'w') as devnull:
             stdin = stdout = stderr = devnull
             for tap in tap_list:
@@ -62,6 +65,21 @@ class Brew(dotbot.Plugin):
                     if result != 0:
                         log.warning('Failed to install [%s]' % package)
                         return False
+            return True
+
+    def _install_bundle(self, brew_files):
+        cwd = self._context.base_directory()
+        log = self._log
+        with open(os.devnull, 'w') as devnull:
+            stdin = stdout = stderr = devnull
+            for f in brew_files:
+                log.info("Installing from file %s" % f)
+                cmd = "brew bundle --file=%s" % f            
+                result = subprocess.call(cmd, shell=True, cwd=cwd)
+
+                if result != 0:
+                    log.warning('Failed to install file [%s]' % tap)
+                    return False
             return True
 
     def _bootstrap(self, cmd):
