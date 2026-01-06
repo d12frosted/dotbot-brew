a3f9bc5b32e06c35fd2c8855084e36c2cf176fc0
Boris Buliga <boris@d12frosted.io>
2026-01-06 09:02:37 +0200

    refactor: convert to f-strings
    Use modern f-string formatting instead of % operator.

--- brew.py
+++ brew.py
@@ -33,16 +33,16 @@ class Brew(dotbot.Plugin):
         if directive == self._servicesDirective:
             self._bootstrap_brew()
             return self._start_services(data)
-        raise ValueError('Brew cannot handle directive %s' % directive)
+        raise ValueError(f'Brew cannot handle directive {directive}')
 
     def _tap(self, tap_list):
         cwd = self._context.base_directory()
         for tap in tap_list:
-            self._log.info("Tapping %s" % tap)
-            cmd = "brew tap %s" % tap
+            self._log.info(f"Tapping {tap}")
+            cmd = f"brew tap {tap}"
             result = subprocess.call(cmd, shell=True, cwd=cwd)
             if result != 0:
-                self._log.warning('Failed to tap [%s]' % tap)
+                self._log.warning(f'Failed to tap [{tap}]')
                 return False
         return True
 
@@ -58,41 +58,41 @@ class Brew(dotbot.Plugin):
         cwd = self._context.base_directory()
         for package in packages_list:
             if install_cmd == 'brew install':
-                check_cmd = "brew list --versions %s" % package
+                check_cmd = f"brew list --versions {package}"
             else:
-                check_cmd = "brew list --cask --versions %s" % package
+                check_cmd = f"brew list --cask --versions {package}"
             already_installed = subprocess.call(
                 check_cmd, shell=True, cwd=cwd,
                 stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL
             ) == 0
             if not already_installed:
-                self._log.info("Installing %s" % package)
-                cmd = "%s %s" % (install_cmd, package)
+                self._log.info(f"Installing {package}")
+                cmd = f"{install_cmd} {package}"
                 result = subprocess.call(cmd, shell=True, cwd=cwd)
                 if result != 0:
-                    self._log.warning('Failed to install [%s]' % package)
+                    self._log.warning(f'Failed to install [{package}]')
                     return False
         return True
 
     def _install_bundle(self, brew_files):
         cwd = self._context.base_directory()
         for f in brew_files:
-            self._log.info("Installing from file %s" % f)
-            cmd = "brew bundle --verbose --file=%s" % f
+            self._log.info(f"Installing from file {f}")
+            cmd = f"brew bundle --verbose --file={f}"
             result = subprocess.call(cmd, shell=True, cwd=cwd)
             if result != 0:
-                self._log.warning('Failed to install file [%s]' % f)
+                self._log.warning(f'Failed to install file [{f}]')
                 return False
         return True
 
     def _start_services(self, services_list):
         cwd = self._context.base_directory()
         for service in services_list:
-            self._log.info("Starting service %s" % service)
-            cmd = "brew services start %s" % service
+            self._log.info(f"Starting service {service}")
+            cmd = f"brew services start {service}"
             result = subprocess.call(cmd, shell=True, cwd=cwd)
             if result != 0:
-                self._log.warning('Failed to start service [%s]' % service)
+                self._log.warning(f'Failed to start service [{service}]')
                 return False
         self._log.info('All services have been started')
         return True
